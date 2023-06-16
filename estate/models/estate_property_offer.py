2eddc696181d518b87a527ea50b97a20c7b31716
Cris-aian Vergara <cris.vergara@achievewithoutborders.com>
2023-06-16 18:11:12 +0800

    computed fields and actions

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,7 @@
-from odoo import models, fields
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, models, fields
+from odoo.exceptions import UserError
 
 
 class EstatePropertyOffer(models.Model):
@@ -17,3 +20,45 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
+
+    # Computed fields
+    validity = fields.Integer("Validity (days)", default=7)
+    date_deadline = fields.Date("Deadline", compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for offer in self:
+            date = offer.create_date.date() if offer.create_date else fields.Date.today()
+            offer.date_deadline = date + relativedelta(days=offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            date = offer.create_date.date() if offer.create_date else fields.Date.today()
+            offer.validity = (offer.date_deadline - date).days
+
+    # Actions
+    def action_accept(self):
+        if "accepted" in self.mapped("property_id.offer_ids.state"):
+            raise UserError("An offer as already been accepted.")
+        self.write(
+            {
+                "state": "accepted",
+            }
+        )
+        return self.mapped("property_id").write(
+            {
+                "state": "offer_accepted",
+                "selling_price": self.price,
+                "buyer_id": self.partner_id.id,
+            }
+        )
+
+    def action_refuse(self):
+        return self.write(
+            {
+                "state": "refused",
+            }
+        )
+        
+
+
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -2,7 +2,7 @@ from odoo import fields, models
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
-    _description = "Reals Estate Property Type"
+    _description = "Real Estate Property Type"
 
 
     name = fields.Char("Type", required=True)
